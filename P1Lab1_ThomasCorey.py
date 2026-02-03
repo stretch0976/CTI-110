9e3ca9c3522bb13fc976ea59131f4b50dd12aac1
stretch0976 <stretch0976.ct@gmail.com>
2026-02-03 11:38:12 -0500

    P1Lab1

---
+++ P1Lab1_ThomasCorey.py
@@ -0,0 +1,12 @@
+# Corey Thomas
+# 03 Feb 26
+# P1lab1.py
+# input and print
+
+print()
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print()
+
+print("Hello, ",first_name,last_name +"!","Welcome to CTI-110")
+print()
